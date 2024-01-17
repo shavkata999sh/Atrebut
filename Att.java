62ca6fd9d583c3933a9b4855bf1b5bdebdd6b193
Shavkat <156001276+shavkata999sh@users.noreply.github.com>
2024-01-17 12:36:45 +0500

    Add files via upload

---
+++ Att.java
@@ -0,0 +1,14 @@
+public class Att {
+        int a=3;
+        double b=4;
+        long c=5;
+        double yuz=(a*b)/2;
+        double perimetr = a+b+c;
+        public static void main(String[] args){
+            Att att  = new Att();
+            System.out.println("Yuza=" + att.yuz);
+            System.out.println("perimetr=" + att.perimetr);
+        }
+    }
+
+
